120be315d6b859f2b1e3b18eb8f3262afd70a0ce
xinxin68 <125354159+xinxin68@users.noreply.github.com>
2023-04-20 09:55:36 +0800

    Delete docker/challenge-docker-image-registry directory

--- docker/challenge-docker-image-registry/verify2.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Check if the user has tagged the image with a custom name
-if [ "$(docker images | grep 'ubuntu:tag')" ]; then
-  echo "Step 2: docker tag - Done"
-else
-  echo "Step 2: docker tag - Not Done"
-fi
--- docker/challenge-docker-image-registry/verify3.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Check if the user has logged in to a Docker registry
-if [ "$(docker ps -a | grep 'login')" ]; then
-  echo "Step 3: docker login - Done"
-else
-  echo "Step 3: docker login - Not Done"
-fi
